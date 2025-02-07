5b8d6c5f3a67eb5fa6f9de755c586d5501de841d
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2025-02-07 17:56:20 -0300

    Criando novo serviço

---
+++ src/main/java/com/matheusmaciel/service/notification/NotificationController.java
@@ -0,0 +1,17 @@
+package com.matheusmaciel.service.notification;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notification")
+public class NotificationController {
+  
+  @PostMapping
+  public  ResponseEntity<Void> sendNotification(@RequestBody NotificationRequest notificationRequest) {
+      return ResponseEntity.ok().build();
+  }
+}
---
+++ src/main/java/com/matheusmaciel/service/notification/NotificationRequest.java
@@ -0,0 +1,4 @@
+package com.matheusmaciel.service.notification;
+
+public record NotificationRequest(String message, String email){
+};
