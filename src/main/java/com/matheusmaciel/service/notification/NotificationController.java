83b12fc5323a859b75cf7a65607fc2e09219c4a5
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2025-02-08 11:35:29 -0300

    finalizei a configuração dos microserviços
    Signed-off-by: srmatheusmaciel <srmatheusmaciel@hotmail.com>

--- src/main/java/com/matheusmaciel/service/notification/NotificationController.java
+++ src/main/java/com/matheusmaciel/service/notification/NotificationController.java
@@ -12,6 +12,7 @@ public class NotificationController {
   
   @PostMapping
   public  ResponseEntity<Void> sendNotification(@RequestBody NotificationRequest notificationRequest) {
+      System.out.println(notificationRequest.message());
       return ResponseEntity.ok().build();
   }
 }
